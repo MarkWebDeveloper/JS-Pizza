859279046bbcd06af46866f36d48ff609c8019ca
Mark-Kil <markkilcanarias@gmail.com>
2023-10-30 10:32:31 +0100

    functioning

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import promptSync from 'prompt-sync';
 import {makeSelectedPizza} from './modules/makePizza.js';
+import { pizzas } from './modules/pizzas.js';
 
 const prompt = promptSync()
 export const pizzaName = prompt("Qué pizza quiere? ")
-export const PizzaIngs = Object.values(pizzaName)[1];
+const selectedPizzaObj = pizzas.find(o => o.name === pizzaName);
+export const PizzaIngs = Object.values(selectedPizzaObj)[1];
 
 if (pizzaName === '') {
     console.log('Por favor, elija una de las pizzas: mediterranea, pepperoni, jamón y queso o cuatro quesos')
